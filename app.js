ed50baaebce86cbf929931d5a97fbe6c6b9d763d
heshamelmasry77 <heshamelmasry@outlook.com>
2017-02-02 11:24:42 +0200

    end points is set it up
    The starting code for my application.
    
    I can get make/models,
    /make
    /model

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var app = require('./config');
 var PORT = app.get('port');
 var PConfig = require('./prismic-configuration');
 var request = require('request');
+var express = require('express');
+var app = express();
 
 function handleError(err, req, res) {
   if (err.status == 404) {
@@ -34,30 +36,100 @@ function api(req, res) {
     req: req
   });
 }
+// GETTING MODEL STEVEN VERSION
+
+// app.get('/make/:make', function(req, res) {
+//   api(req, res).then(function(api) {
+//     return api.getByUID('make', req.params.make);
+//   }).then(function(document) {
+//     res.setHeader('Content-Type', 'application/json');
+//     res.send({
+//       id: document.id,
+//       uid: document.uid,
+//       title: document.data['make.title'].value[0].text
+//     });
+//   });
+// });
+
+// GETTING MAKE HESHAM VERSION
 
 app.get('/make/:make', function(req, res) {
   api(req, res).then(function(api) {
     return api.getByUID('make', req.params.make);
   }).then(function(document) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send({
-      id: document.id,
-      uid: document.uid,
-      title: document.data['make.title'].value[0].text
-    });
+    // console.log(document);
+    var info = '';
+    // res.setHeader('Content-Type', 'application/json');
+    info += `
+    <ul>
+    <li>
+      <h2>${document.id}</h2>
+      <p>${document.data['make.title'].value[0].text}</p>
+    </li>
+    </ul>
+    `;
+    res.send(`
+      <h1>carzar</h1>
+      ${info}
+  `);
   });
+
 });
 
+// GETTING MODEL STEVEN VERSION
+
+// app.get('/model/:model', function(req, res) {
+//   api(req, res).then(function(api) {
+//     return api.getByUID('model', req.params.model);
+//   }).then(function(document) {
+//     res.setHeader('Content-Type', 'application/json');
+//     res.send(JSON.stringify(document));
+//   });
+// });
+
+// GETTING MODEL HESHAM VERSION
 app.get('/model/:model', function(req, res) {
   api(req, res).then(function(api) {
     return api.getByUID('model', req.params.model);
   }).then(function(document) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(document));
+    // res.setHeader('Content-Type', 'application/json');
+    // console.log(document);
+      var info = '';
+
+      info += `
+      <ul>
+      <li>
+        <h2>${document.id}</h2>
+        <p>${document.fragments['model.description'].blocks[0].text}</p>
+        <img src="${document.fragments['model.banner-image'].url}">
+      </li>
+      </ul>
+      `;
+    res.send(
+      `
+        <h1>carzar</h1>
+        ${info}
+      `
+      // JSON.stringify(document)
+  );
   });
 });
 
+// GETTING ALL THE MAKE MODELS eg: make/volkswagen/models
 app.get('/make/:make/models', function(req, res) {
+  api(req, res).then(function(api) {
+    return api.query([
+      Prismic.Predicates.at("document.type", "model"),
+      Prismic.Predicates.at("my.model.make", req.params.make)
+    ]);
+  }).then(function(document) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(document.results));
+  });
+});
+
+// GETTING MAKE/MODEL  eg:  volkswagen/volkswagen-polo-classic
+app.get('/make/:make/:model', function(req, res) {
   api(req, res).then(function(api) {
     return api.query([
       Prismic.Predicates.at("document.type", "model"),
